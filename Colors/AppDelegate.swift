a81b732e1f207feb6169647bb33ad7120e8fcbc7
Kaunteya <k.suryawanshi@gmail.com>
2019-05-04 11:30:58 +0530

    applicationShouldTerminateAfterLastWindowClosed => true

--- Colors/AppDelegate.swift
+++ Colors/AppDelegate.swift
@@ -19,5 +19,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             ]
         )
     }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 }
 
